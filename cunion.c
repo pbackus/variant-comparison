4ca7a9b10d06ad774268b1ea48c208b534c21978
Paul Backus <snarwin@gmail.com>
2019-10-04 17:04:06 -0400

    Use separate visitor functions in C version

--- cunion.c
+++ cunion.c
@@ -42,19 +42,30 @@ struct variant
 	enum tag tag;
 };
 
+int visit_T0() { return 3; }
+int visit_T1() { return 5; }
+int visit_T2() { return 8; }
+int visit_T3() { return 13; }
+int visit_T4() { return 21; }
+int visit_T5() { return 34; }
+int visit_T6() { return 55; }
+int visit_T7() { return 89; }
+int visit_T8() { return 144; }
+int visit_T9() { return 233; }
+
 int do_visit(struct variant v)
 {
 	switch(v.tag) {
-		case TAG_T0: return 3;
-		case TAG_T1: return 5;
-		case TAG_T2: return 8;
-		case TAG_T3: return 13;
-		case TAG_T4: return 21;
-		case TAG_T5: return 34;
-		case TAG_T6: return 55;
-		case TAG_T7: return 89;
-		case TAG_T8: return 144;
-		case TAG_T9: return 233;
+		case TAG_T0: return visit_T0();
+		case TAG_T1: return visit_T1();
+		case TAG_T2: return visit_T2();
+		case TAG_T3: return visit_T3();
+		case TAG_T4: return visit_T4();
+		case TAG_T5: return visit_T5();
+		case TAG_T6: return visit_T6();
+		case TAG_T7: return visit_T7();
+		case TAG_T8: return visit_T8();
+		case TAG_T9: return visit_T9();
 		default: assert(0);
 	}
 }
