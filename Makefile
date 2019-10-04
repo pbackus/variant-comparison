da5110dc061a8d714bb108d077a4dc7790e5b413
Paul Backus <snarwin@gmail.com>
2019-10-04 15:33:51 -0400

    Add C version

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 CXX = clang++-8 -std=c++17 -stdlib=libc++
 DC = ldc2
+CC = clang-8 -std=c99
 OBJDUMP_FLAGS = -dwr --no-show-raw-insn
 
-all: cppvariant.txt dsumtype.txt dalgebraic.txt
+all: cunion.txt cppvariant.txt dsumtype.txt dalgebraic.txt
 
 clean:
 	rm *.o *.txt
+
+cunion.o: cunion.c
+	$(CC) -c -O2 -o $@ $<
 
 cppvariant.o: cppvariant.cpp
 	$(CXX) -c -O2 -o $@ $<
@@ -15,6 +19,9 @@ dsumtype.o: dsumtype.d
 
 dalgebraic.o: dalgebraic.d
 	$(DC) -c -O2 -of=$@ $<
+
+cunion.txt: cunion.o
+	objdump $(OBJDUMP_FLAGS) $< > $@
 
 cppvariant.txt: cppvariant.o
 	objdump $(OBJDUMP_FLAGS) $< | c++filt > $@
---
+++ cunion.c
@@ -0,0 +1,60 @@
+#include <assert.h>
+
+struct T0 {};
+struct T1 {};
+struct T2 {};
+struct T3 {};
+struct T4 {};
+struct T5 {};
+struct T6 {};
+struct T7 {};
+struct T8 {};
+struct T9 {};
+
+enum tag
+{
+	TAG_T0,
+	TAG_T1,
+	TAG_T2,
+	TAG_T3,
+	TAG_T4,
+	TAG_T5,
+	TAG_T6,
+	TAG_T7,
+	TAG_T8,
+	TAG_T9
+};
+
+struct variant
+{
+	union {
+		struct T0 t0;
+		struct T1 t1;
+		struct T2 t2;
+		struct T3 t3;
+		struct T4 t4;
+		struct T5 t5;
+		struct T6 t6;
+		struct T7 t7;
+		struct T8 t8;
+		struct T9 t9;
+	} data;
+	enum tag tag;
+};
+
+int do_visit(struct variant v)
+{
+	switch(v.tag) {
+		case TAG_T0: return 3;
+		case TAG_T1: return 5;
+		case TAG_T2: return 8;
+		case TAG_T3: return 13;
+		case TAG_T4: return 21;
+		case TAG_T5: return 34;
+		case TAG_T6: return 55;
+		case TAG_T7: return 89;
+		case TAG_T8: return 144;
+		case TAG_T9: return 233;
+		default: assert(0);
+	}
+}
